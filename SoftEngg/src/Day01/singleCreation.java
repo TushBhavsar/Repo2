5592e4103a54216d90545c68be9c74acdeb1e47c
root <tusharbhavsar119@gmail.com>
2022-12-21 10:38:38 +0530

    my first commit

---
+++ SoftEngg/src/Day01/singleCreation.java
@@ -0,0 +1,32 @@
+package Day01;
+
+import java.util.Scanner;
+
+public class singleCreation {
+	
+	static class Node{
+	int data;
+	Node next;
+	
+	Node(int data,Node next){
+		this.data=data;
+		this.next=next;
+	}
+}
+	
+	Node head=null;
+	
+	public void creation() {
+		int data;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the values");
+		data=sc.nextInt();
+		
+		}
+	public void transverser() {
+		
+	}
+	}
+
+
+
